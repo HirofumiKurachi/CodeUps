b1ec0f3eb41fcb9ff20d7bb56298da48855782ec
{kurachihirofumi} <{kuratch37@gmail.com}>
2024-02-11 16:56:19 +0900

    ドロワー修正

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,24 @@ jQuery(function ($) {
     $(".js-drawer").fadeToggle();
   });
 });
+
+// resizeイベント
+$(window).resize(function () {
+  if (window.matchMedia("(min-width: 768px)").matches) {
+      closeDrawer();
+    }
+  } );
+
+
+// 新しい関数: openDrawer
+function openDrawer() {
+  $(".js-drawer").fadeIn();
+  $(".js-hamburger").addClass("is-open");
+}
+
+// 新しい関数: closeDrawer
+function closeDrawer() {
+  $(".js-drawer").fadeOut();
+  $(".js-hamburger").removeClass("is-open");
+  $(".js-hamburger").removeClass("is-active");
+}
