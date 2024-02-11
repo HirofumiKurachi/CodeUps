668006e75eac0c5c81af8aa6cbdf64b8006a08d4
{kurachihirofumi} <{kuratch37@gmail.com}>
2024-02-12 06:18:57 +0900

    mv追加

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,60 @@
 
-jQuery(function ($) {
-  //ハンバーガーメニュー
-  $(".js-hamburger,.js-drawer,.js-drawer a").click(function () {
-    $(".js-hamburger").toggleClass("is-active");
-    $(".js-drawer").fadeToggle();
+$(document).ready(function () {
+  // ハンバーガーメニュー
+  $(function () {
+    $(".js-hamburger").click(function () {
+      $(this).toggleClass("is-active");
+      if ($(this).hasClass("is-active")) {
+        openDrawer();
+      } else {
+        closeDrawer();
+      }
+    });
+
+    //backgroundまたはページ内リンクをクリックで閉じる
+    $(".js-drawer a[href]").on("click", function (event) {
+      closeDrawer();
+    });
+
+    //resizeイベント
+    $(window).resize(function () {
+      if (window.matchMedia("(min-width: 768px)").matches) {
+        closeDrawer();
+      }
+    });
+  });
+
+  function openDrawer() {
+    $(".js-drawer").fadeIn();
+    $(".js-hamburger").addClass("is-active");
+  }
+
+  function closeDrawer() {
+    $(".js-drawer").fadeOut();
+    $(".js-hamburger").removeClass("is-active");
+  }
+
+  // MV過ぎたらヘッダー背景色変化
+  $(window).on("scroll", function () {
+    mvHeight = $(".js-mv").height();
+    if ($(window).scrollTop() > mvHeight) {
+      $(".js-header").addClass("is-scroll");
+    } else {
+      $(".js-header").removeClass("is-scroll");
+    }
+  });
+
+  // スライダー
+  const mv_swiper = new Swiper(".mv__slider.swiper", {
+    loop: true,
+    speed: 2000,
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true,
+    },
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
   });
 });
-
-// resizeイベント
-$(window).resize(function () {
-  if (window.matchMedia("(min-width: 768px)").matches) {
-      closeDrawer();
-    }
-  } );
-
-
-// 新しい関数: openDrawer
-function openDrawer() {
-  $(".js-drawer").fadeIn();
-  $(".js-hamburger").addClass("is-open");
-}
-
-// 新しい関数: closeDrawer
-function closeDrawer() {
-  $(".js-drawer").fadeOut();
-  $(".js-hamburger").removeClass("is-open");
-  $(".js-hamburger").removeClass("is-active");
-}
