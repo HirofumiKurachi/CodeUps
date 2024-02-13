69330df46b57a0a4ee8b3c2ed56590f5e3c19d86
{kurachihirofumi} <{kuratch37@gmail.com}>
2024-02-14 06:13:46 +0900

    js修正

--- src/js/script.js
+++ src/js/script.js
@@ -1,38 +1,39 @@
 
-$(document).ready(function () {
-  // ハンバーガーメニュー
-  $(function () {
-      $(".js-hamburger").click(function () {
-      $(this).toggleClass("is-active");
-      if ($(this).hasClass("is-active")) {
-          openDrawer();
-      } else {
-          closeDrawer();
-      }
-      });
+jQuery(function ($) {
+    // ハンバーガーメニュー
+    $(function () {
+        $(".js-hamburger").click(function () {
+        $(this).toggleClass("is-open");
+        if ($(this).hasClass("is-open")) {
+            openDrawer();
+        } else {
+            closeDrawer();
+        }
+        });
 
-      //backgroundまたはページ内リンクをクリックで閉じる
-      $(".js-drawer a[href]").on("click", function (event) {
-          closeDrawer();
-      });
+        //backgroundまたはページ内リンクをクリックで閉じる
+        $(".js-drawer a[href]").on("click", function (event) {
+            closeDrawer();
+        });
 
-  //resizeイベント
-  $(window).resize(function () {
-      if (window.matchMedia("(min-width: 767px)").matches) {
-          closeDrawer();
-      }
-      });
-  });
+        //resizeイベント
+        $(window).resize(function () {
+            if (window.matchMedia("(min-width: 768px)").matches) {
+                closeDrawer();
+            }
+        });
+    });
 
-  function openDrawer() {
-      $(".js-drawer").fadeIn();
-      $(".js-hamburger").addClass("is-active");
-  }
+    function openDrawer() {
+        $(".js-drawer").fadeIn();
+        $(".js-hamburger").addClass("is-open");
+    }
 
-  function closeDrawer() {
-      $(".js-drawer").fadeOut();
-      $(".js-hamburger").removeClass("is-active");
-  }
+    function closeDrawer() {
+        $(".js-drawer").fadeOut();
+        $(".js-hamburger").removeClass("is-open");
+    }
+
 
   // MV過ぎたらヘッダー背景色変化
   $(window).on("scroll", function () {
