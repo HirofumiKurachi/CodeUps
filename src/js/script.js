78b6349c0496facef87b99eae077c9aa8f003d99
{kurachihirofumi} <{kuratch37@gmail.com}>
2024-02-16 06:05:23 +0900

    キャンペーンスライダー実装途中

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,76 @@
 
+// jQuery(function ($) {
+//     // ハンバーガーメニュー
+//     $(function () {
+//         $(".js-hamburger").click(function () {
+//         $(this).toggleClass("is-active");
+//         if ($(this).hasClass("is-active")) {
+//             openDrawer();
+//         } else {
+//             closeDrawer();
+//         }
+//         });
+
+//         //backgroundまたはページ内リンクをクリックで閉じる
+//         $(".js-drawer a[href]").on("click", function (event) {
+//             closeDrawer();
+//         });
+
+//         //resizeイベント
+//         $(window).resize(function () {
+//             if (window.matchMedia("(min-width: 767px)").matches) {
+//                 closeDrawer();
+//             }
+//         });
+//     });
+
+//     function openDrawer() {
+//         $(".js-drawer").fadeIn();
+//         $(".js-hamburger").addClass("is-active");
+//     }
+
+//     function closeDrawer() {
+//         $(".js-drawer").fadeOut();
+//         $(".js-hamburger").removeClass("is-active");
+//     }
+
+
+//   // MV過ぎたらヘッダー背景色変化
+//   $(window).on("scroll", function () {
+//      mvHeight = $(".js-mv").height();
+//      if ($(window).scrollTop() > mvHeight) {
+//          $(".js-header").addClass("is-scroll");
+//      } else {
+//          $(".js-header").removeClass("is-scroll");
+//      }
+//   });
+
+//   // スライダー
+//   const mv_swiper = new Swiper(".mv__slider.swiper", {
+//     loop: true,
+//     speed: 2000,
+//     effect: "fade",
+//     fadeEffect: {
+//         crossFade: true,
+//     },
+//     autoplay: {
+//         delay: 4000,
+//         disableOnInteraction: false,
+//         },
+//     });
+
+// });
+
 jQuery(function ($) {
     // ハンバーガーメニュー
     $(function () {
         $(".js-hamburger").click(function () {
-        $(this).toggleClass("is-active");
-        if ($(this).hasClass("is-active")) {
-            openDrawer();
-        } else {
-            closeDrawer();
-        }
+            $(this).toggleClass("is-active");
+            if ($(this).hasClass("is-active")) {
+                openDrawer();
+            } else {
+                closeDrawer();
+            }
         });
 
         //backgroundまたはページ内リンクをクリックで閉じる
@@ -35,40 +97,36 @@ jQuery(function ($) {
     }
 
 
-  // MV過ぎたらヘッダー背景色変化
-  $(window).on("scroll", function () {
-     mvHeight = $(".js-mv").height();
-     if ($(window).scrollTop() > mvHeight) {
-         $(".js-header").addClass("is-scroll");
-     } else {
-         $(".js-header").removeClass("is-scroll");
-     }
-  });
+    // MV過ぎたらヘッダー背景色変化
+    $(window).on("scroll", function () {
+        mvHeight = $(".js-mv").height();
+        if ($(window).scrollTop() > mvHeight) {
+            $(".js-header").addClass("is-scroll");
+        } else {
+            $(".js-header").removeClass("is-scroll");
+        }
+    });
 
-  // スライダー
-  const mv_swiper = new Swiper(".mv__slider.swiper", {
-    loop: true,
-    speed: 2000,
-    effect: "fade",
-    fadeEffect: {
-        crossFade: true,
-    },
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
+    // スライダーの初期化
+    const mv_swiper = new Swiper(".mv__slider.swiper", {
+        loop: true,
+        speed: 2000,
+        effect: "fade",
+        fadeEffect: {
+            crossFade: true,
+        },
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
         },
     });
 
-    // const mv_swiper = new Swiper(".mv__slider.swiper", {
-    //     loop: true,
-    //     speed: 2000,
-    //     effect: "fade",
-    //     fadeEffect: {
-    //         crossFade: true,
-    //     },
-    //     autoplay: {
-    //         delay: 4000,
-    //         disableOnInteraction: false,
-    //         },
-    //     });
+    // キャンペーンスライダーの初期化
+    const campaign_swiper = new Swiper(".campaign__slider.swiper", {
+        loop: true,
+        autoplay: {
+            delay: 5000, // 自動再生の間隔（ミリ秒）
+        },
+        // 他のオプションを追加
+    });
 });
