7a7aa0e7b6da52f728dad987983825649f0b0fa2
{kurachihirofumi} <{kuratch37@gmail.com}>
2024-02-19 20:57:23 +0900

    色々修正

--- src/js/script.js
+++ src/js/script.js
@@ -61,29 +61,163 @@
 
 // });
 
+// jQuery(function ($) {
+//     // ハンバーガーメニュー
+//     $(function () {
+//         $(".js-hamburger").click(function () {
+//             $(this).toggleClass("is-active");
+//             if ($(this).hasClass("is-active")) {
+//                 openDrawer();
+//             } else {
+//                 closeDrawer();
+//             }
+//         });
+
+//         //backgroundまたはページ内リンクをクリックで閉じる
+//         $(".js-drawer a[href]").on("click", function (event) {
+//             closeDrawer();
+//         });
+
+//         //resizeイベント
+//         $(window).resize(function () {
+//             if (window.matchMedia("(min-width: 768px)").matches) {
+//                 closeDrawer();
+//             }
+//         });
+//     });
+
+//     function openDrawer() {
+//         $(".js-drawer").fadeIn();
+//         $(".js-hamburger").addClass("is-active");
+//     }
+
+//     function closeDrawer() {
+//         $(".js-drawer").fadeOut();
+//         $(".js-hamburger").removeClass("is-active");
+//     }
+
+
+//     // MV過ぎたらヘッダー背景色変化
+//     $(window).on("scroll", function () {
+//         mvHeight = $(".js-mv").height();
+//         if ($(window).scrollTop() > mvHeight) {
+//             $(".js-header").addClass("is-scroll");
+//         } else {
+//             $(".js-header").removeClass("is-scroll");
+//         }
+//     });
+
+//     // function initializeSwiper() {
+//     //     const mv_swiper = new Swiper(".mv__slider.swiper", {
+//     //         loop: true,
+//     //         speed: 2000,
+//     //         effect: "fade",
+//     //         fadeEffect: {
+//     //             crossFade: true,
+//     //         },
+//     //         autoplay: {
+//     //             delay: 4000,
+//     //             disableOnInteraction: false,
+//     //         },
+//     //     });
+//     // }
+//     // initializeSwiper();
+
+//     // function initializeCampaignSwiper() {
+//     //     const campaign_swiper = new Swiper(".campaign__slider.swiper", {
+//     //         loop: true,
+//     //         autoplay: {
+//     //             delay: 5000, // 自動再生の間隔（ミリ秒）
+//     //         },
+//     //         // 他のオプションを追加
+//     //     });
+//     // }
+//     // initializeCampaignSwiper();
+
+//     // スライダーの初期化
+//     // const mv_swiper = new Swiper(".mv__slider.swiper", {
+//     //     loop: true,
+//     //     speed: 2000,
+//     //     effect: "fade",
+//     //     fadeEffect: {
+//     //         crossFade: true,
+//     //     },
+//     //     autoplay: {
+//     //         delay: 4000,
+//     //         disableOnInteraction: false,
+//     //     },
+//     //   });
+
+//     // // キャンペーンスライダーの初期化
+//     // const campaign_swiper = new Swiper(".campaign__slider.swiper", {
+//     //     loop: true,
+//     //     autoplay: {
+//     //         delay: 5000, // 自動再生の間隔（ミリ秒）
+//     //     },
+//     //     // 他のオプションを追加
+//     // });
+//     let campaign_swiper; // グローバルスコープで campaign_swiper を定義
+
+// function initializeSwiper() {
+//     const mv_swiper = new Swiper(".mv__slider", {
+//         loop: true,
+//         speed: 2000,
+//         effect: "fade",
+//         fadeEffect: {
+//             crossFade: true,
+//         },
+//         autoplay: {
+//             delay: 4000,
+//             disableOnInteraction: false,
+//         },
+//     });
+// }
+
+// initializeSwiper();
+
+
+// function initializeCampaignSwiper() {
+//     campaign_swiper = new Swiper(".campaign-slider", {
+//         loop: true,
+//         grabCursor: true,
+//         slidesPerView: 3,
+//         spaceBetween: 40,
+//         768: {
+//             spaceBetween: 16,
+//             slidesPerView: 3,
+//           },
+//           navigation: {
+//             nextEl: '.swiper-button-next',
+//             prevEl: '.swiper-button-prev',
+//           },
+//     });
+// }
+
+// initializeCampaignSwiper();
+
+// });
+
 jQuery(function ($) {
     // ハンバーガーメニュー
-    $(function () {
-        $(".js-hamburger").click(function () {
-            $(this).toggleClass("is-active");
-            if ($(this).hasClass("is-active")) {
-                openDrawer();
-            } else {
-                closeDrawer();
-            }
-        });
-
-        //backgroundまたはページ内リンクをクリックで閉じる
-        $(".js-drawer a[href]").on("click", function (event) {
+    $(".js-hamburger").click(function () {
+        $(this).toggleClass("is-active");
+        if ($(this).hasClass("is-active")) {
+            openDrawer();
+        } else {
             closeDrawer();
-        });
+        }
+    });
 
-        //resizeイベント
-        $(window).resize(function () {
-            if (window.matchMedia("(min-width: 768px)").matches) {
-                closeDrawer();
-            }
-        });
+    //backgroundまたはページ内リンクをクリックで閉じる
+    $(".js-drawer a[href]").on("click", function (event) {
+        closeDrawer();
+    });
+
+    //resizeイベント
+    $(window).resize(function () {
+        if (window.matchMedia("(min-width: 768px)").matches) {
+            closeDrawer();
+        }
     });
 
     function openDrawer() {
@@ -96,7 +230,6 @@ jQuery(function ($) {
         $(".js-hamburger").removeClass("is-active");
     }
 
-
     // MV過ぎたらヘッダー背景色変化
     $(window).on("scroll", function () {
         mvHeight = $(".js-mv").height();
@@ -107,85 +240,42 @@ jQuery(function ($) {
         }
     });
 
-    // function initializeSwiper() {
-    //     const mv_swiper = new Swiper(".mv__slider.swiper", {
-    //         loop: true,
-    //         speed: 2000,
-    //         effect: "fade",
-    //         fadeEffect: {
-    //             crossFade: true,
-    //         },
-    //         autoplay: {
-    //             delay: 4000,
-    //             disableOnInteraction: false,
-    //         },
-    //     });
-    // }
-    // initializeSwiper();
-
-    // function initializeCampaignSwiper() {
-    //     const campaign_swiper = new Swiper(".campaign__slider.swiper", {
-    //         loop: true,
-    //         autoplay: {
-    //             delay: 5000, // 自動再生の間隔（ミリ秒）
-    //         },
-    //         // 他のオプションを追加
-    //     });
-    // }
-    // initializeCampaignSwiper();
-
-    // スライダーの初期化
-    // const mv_swiper = new Swiper(".mv__slider.swiper", {
-    //     loop: true,
-    //     speed: 2000,
-    //     effect: "fade",
-    //     fadeEffect: {
-    //         crossFade: true,
-    //     },
-    //     autoplay: {
-    //         delay: 4000,
-    //         disableOnInteraction: false,
-    //     },
-    //   });
-
-    // // キャンペーンスライダーの初期化
-    // const campaign_swiper = new Swiper(".campaign__slider.swiper", {
-    //     loop: true,
-    //     autoplay: {
-    //         delay: 5000, // 自動再生の間隔（ミリ秒）
-    //     },
-    //     // 他のオプションを追加
-    // });
     let campaign_swiper; // グローバルスコープで campaign_swiper を定義
 
-function initializeSwiper() {
-    const mv_swiper = new Swiper(".mv__slider", {
-        loop: true,
-        speed: 2000,
-        effect: "fade",
-        fadeEffect: {
-            crossFade: true,
-        },
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false,
-        },
-    });
-}
+    function initializeSwiper() {
+        const mv_swiper = new Swiper(".mv__slider", {
+            loop: true,
+            speed: 2000,
+            effect: "fade",
+            fadeEffect: {
+                crossFade: true,
+            },
+            autoplay: {
+                delay: 4000,
+                disableOnInteraction: false,
+            },
+        });
+    }
 
-initializeSwiper();
+    initializeSwiper();
 
+    function initializeCampaignSwiper() {
+        campaign_swiper = new Swiper(".campaign-slider", {
+            loop: true,
+            grabCursor: true,
+            slidesPerView: 3,
+            spaceBetween: 40,
+            768: {
+                spaceBetween: 16,
+                slidesPerView: 3,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    }
 
-function initializeCampaignSwiper() {
-    campaign_swiper = new Swiper(".campaign__slider", {
-        loop: true,
-        autoplay: {
-            delay: 5000, // 自動再生の間隔（ミリ秒）
-        },
-        // 他のオプションを追加
-    });
-}
-
-initializeCampaignSwiper();
-
+    initializeCampaignSwiper();
 });
+
