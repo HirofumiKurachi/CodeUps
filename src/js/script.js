ee222e7c74fbf1232e4cc2f66b0f2eab7229c609
{kurachihirofumi} <{kuratch37@gmail.com}>
2024-02-28 07:51:21 +0900

    meta修正とレスポンシブ対応

--- src/js/script.js
+++ src/js/script.js
@@ -43,6 +43,33 @@ jQuery(function ($) {
         }
     });
 
+
+//     //要素の取得とスピードの設定
+// var box = $('.colorbox'),
+// speed = 700;
+
+// //.colorboxの付いた全ての要素に対して下記の処理を行う
+// box.each(function(){
+// $(this).append('<div class="color"></div>')
+// var color = $(this).find($('.color')),
+// image = $(this).find('img');
+// var counter = 0;
+
+// image.css('opacity','0');
+// color.css('width','0%');
+// //inviewを使って背景色が画面に現れたら処理をする
+// color.on('inview', function(){
+//     if(counter == 0){
+//      $(this).delay(200).animate({'width':'100%'},speed,function(){
+//                image.css('opacity','1');
+//                $(this).css({'left':'0' , 'right':'auto'});
+//                $(this).animate({'width':'0%'},speed);
+//             })
+//         counter = 1;
+//       }
+//  });
+// });
+
     let campaign_swiper; // グローバルスコープで campaign_swiper を定義
 
     function initializeSwiper() {
@@ -73,10 +100,10 @@ jQuery(function ($) {
                     spaceBetween: 40,
                     slidesPerView: 3,
                 },
-                1024: {
-                    spaceBetween: 24,
-                    slidesPerView: auto,
-                },
+                // 1024: {
+                //     spaceBetween: 24,
+                //     slidesPerView: auto,
+                // },
             },
             navigation: {
                 nextEl: '.campaign-swiper__button-next',
