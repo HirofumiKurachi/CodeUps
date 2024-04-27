71ee31eb9efbd3763e98f7edad19e14eed9b350d
{kurachihirofumi} <{kuratch37@gmail.com}>
2024-04-28 08:38:04 +0900

    ブログの人気記事まで作成

--- src/js/script.js
+++ src/js/script.js
@@ -57,18 +57,6 @@ jQuery(function ($) {
   });
 
   //タブ
-// $(function () {
-//   const tabButton = $(".js-tab-button"),
-//     tabContent = $(".js-tab-content");
-//   tabButton.on("click", function () {
-//     let index = tabButton.index(this);
-
-//     tabButton.removeClass("is-active");
-//     $(this).addClass("is-active");
-//     tabContent.removeClass("is-active");
-//     tabContent.eq(index).addClass("is-active");
-//   });
-// });
 $(function () {
   const tabButton = $(".js-tab-button"),
     tabContent = $(".js-tab-content");
